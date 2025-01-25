8d50bb58bdde9d40f6264df44ba448bc813b2ae1
Mileski-Lucas <lucas.m_47@hotmail.com>
2025-01-25 16:46:31 -0300

    Tests finnished. All classes working

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,16 +6,15 @@ class Program
     {
         int opt = 0;
         string proceed = "";
-        
-       
+               
         Console.WriteLine("Select the option for the scripture you want to memorize ");
         
         Console.Write("1 - ");
         Reference ref_01 = new Reference( "John", 3, 16);
-        string scrip_01 = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life";
+        string scrip_01 = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
         
         string[] words_01 = scrip_01.Split(' ');
-        Scripture scripture_01 = new Scripture();
+        Scripture scripture_01 = new Scripture(ref_01.GetDisplayText());
         foreach (string word in words_01)
         {
             scripture_01._words.Add(new Word(word));
@@ -23,10 +22,10 @@ class Program
         Console.WriteLine(ref_01.GetDisplayText());
         Console.Write("2 - ");
         Reference ref_02 = new Reference("Proverbs", 3, 5, 6);
-        string scrip_02 = "Trust in the Lord with all thine heart; and lean not unto thine own understanding.In all thy ways acknowledge him, and he shall direct thy paths";
+        string scrip_02 = "Trust in the Lord with all thine heart; and lean not unto thine own understanding.In all thy ways acknowledge him, and he shall direct thy paths.";
         
         string[] words_02 = scrip_02.Split(' ');
-        Scripture scripture_02 = new Scripture();
+        Scripture scripture_02 = new Scripture(ref_02.GetDisplayText());
         foreach (string word in words_02)
         {
             scripture_02._words.Add(new Word(word));
@@ -35,30 +34,40 @@ class Program
         opt = int.Parse(Console.ReadLine());
         if(opt == 1)
         {
-            while(proceed != "quit")
+            while(proceed != "quit" && !scripture_01.IsCompletelyHidden())
             {
-                Console.Write(ref_01.GetDisplayText());
+                Console.Clear();
+                Console.WriteLine(scripture_01.GetDisplayText());
                 Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
                 proceed = Console.ReadLine();
+                if(proceed != "quit")
+                {
+                    scripture_01.HideRandomWords(3); // Hide 3 random words each time
+                }
                 Console.Clear();
+                Console.WriteLine(scripture_01.GetDisplayText());
+                Console.WriteLine("All words are hidden! Program finished.");
             }
             
         }
         else if(opt == 2)
         {
-            while(proceed != "quit")
+            while(proceed != "quit" && !scripture_02.IsCompletelyHidden())
             {
-                Console.Write(ref_02.GetDisplayText());
+                Console.Clear();
+                Console.WriteLine(scripture_02.GetDisplayText());
                 Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
                 proceed = Console.ReadLine();
+                if(proceed != "quit")
+                {
+                    scripture_02.HideRandomWords(3); // Hide 3 random words each time
+                }
                 Console.Clear();
+                Console.WriteLine(scripture_02.GetDisplayText());
+                Console.WriteLine("All words are hidden! Program finished.");
             }
+
         }
             
     }
-        
-
-        
-
-        
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -2,32 +2,52 @@ using System;
 
 public class Scripture
 {
-    public string _reference;
+    private string _reference;
 
     public List<Word> _words = new List<Word>();
 
-    //public Scripture (Reference reference, string text)
-    //{
-    //    reference = _reference;
-    //}
-
-    public void HideRandomWords(int numberToRide)
+    public Scripture(string reference)
     {
+        _reference = reference;
+    }
 
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide && IsCompletelyHidden() == false)
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
     }
 
     public string GetDisplayText()
     {
-        string text = $"{_reference}/{_reference}";
-
-        return text;
+        string text = "";
+        foreach (var word in _words)
+        {
+            text = text+$"{word.GetDisplayText()} ";
+        }
+        
+        return _reference+$" {text.Trim()}";
 
     }
 
     public bool IsCompletelyHidden()
     {
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
         return true;
     }
-
-    
 }
